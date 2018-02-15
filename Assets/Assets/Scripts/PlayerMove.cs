1d7c20bdc47b71e5c9519db3973bfaff6c9f8e34
onagu71 <34030220+onagu71@users.noreply.github.com>
2018-02-15 13:17:19 +0900

    2/15-1
    ggg

---
+++ Assets/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    Rigidbody2D rigid;
+    Vector3 movement;
+
+    public float maxMoveSpeed = 5f;
+    public float horMoveSpeed = 0f;
+    public float verMoveSpeed = 0f;
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Moving();
+	}
+
+    private void Moving()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        movement = new Vector3(hor, ver, 0);
+        transform.position = Vector3.Lerp(transform.position, movement , Time.deltaTime * maxMoveSpeed);
+
+
+
+        //movement = movement.normalized * moveSpeed * Time.deltaTime;
+        //transform.Translate(movement);
+    }
+}
